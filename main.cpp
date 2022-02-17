4cef698da8f66429df5a420dc234905b4111cd98
snxhz01 <77512847+snxhz01@users.noreply.github.com>
2022-02-17 12:47:37 -0500

    Updated main
    created & assigned all values:  ints, doubles, chars, doubles and strings

--- main.cpp
+++ main.cpp
@@ -6,4 +6,26 @@ using namespace std;
 
 int main(){
 
+int org;
+int length;
+
+double numA;
+double numG;
+double numC;
+double numT;
+
+char a = 'A'; 
+char t = 'T';
+char c = 'C';
+char g = 'G';
+
+double count_a = 0; 
+double count_t = 0;
+double count_c = 0;
+double count_g = 0;
+
+string genStrand; 
+string step;
+int yes; 
+
 }
